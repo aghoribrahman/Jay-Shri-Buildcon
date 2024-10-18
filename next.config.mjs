37a2cf91594b24634cad7382d90a4dc5ad4dcb23
Aghoribrahman <mohit.4033@gmail.com>
2024-10-18 15:03:57 +0530

    light and dark

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // Enables static export mode
-    // Optional: other configurations can go here
-    webpack: (config) => {
-        // Modify the existing config here
-        return config;
-      },
-};
-  
-  export default nextConfig;
+const nextConfig = {};
+
+
+export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={`${outfit.className} antialiased bg-zinc`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
